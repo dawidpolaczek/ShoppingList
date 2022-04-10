009d71036bbc70893142d68628f2e59ea373e1b7
dawidpolaczek <dkpolaczek@gmail.com>
2022-04-10 11:12:25 +0200

    Add ShoppingListDbContext and essential packages.

---
+++ ShoppingList/Data/ShoppingListDbContext.cs
@@ -0,0 +1,16 @@
+﻿#nullable disable
+using Microsoft.EntityFrameworkCore;
+using ShoppingList.Models;
+
+namespace ShoppingList.Data
+{
+    public class ShoppingListDbContext : DbContext
+    {
+        public ShoppingListDbContext(DbContextOptions<ShoppingListDbContext> options)
+            : base(options) { }
+
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
